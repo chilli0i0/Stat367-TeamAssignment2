31b0848a6aef0a7401085ee070c0ce3fdb05110a
yilixia <yilixia@10.141.70.54>
2017-12-01 15:14:33 -0600

    Working on.

--- src/Test_ListImplementations.java
+++ src/Test_ListImplementations.java
@@ -95,19 +95,48 @@ public class Test_ListImplementations {
 		}
 
 		// TODO: name and write additional tests to run on each list.
-		test01_test_name_here(className,list);
-
+		test01_test_addOneItem(className,list);
+		test01_test_addMultipleItems(className,list);
 
 	}
 
 	/** 
-	 * A sample test method.
+	 * Test the method add(E item).
 	 * @param list
 	 */
-	private static void test01_test_name_here(String className, ListADT<String> list) {
-		// TODO Auto-generated method stub
+	private static void test01_test_addOneItem(String className, ListADT<String> list) {
+		System.out.println("Begin to test on method add(E item) of List_" + className);
+		try {
+			System.out.println("Add one Item");
+			list.add("0");
+		} catch (Exception e) {
+			//TODO: Add evaluation. 
+		} finally {
+			System.out.println("Expected: One item is added into the List_" + className);
+		}
 
 	}
-
+	private static void test01_test_addMultipleItems(String className, ListADT<String> list) {
+		System.out.println("Begin to test on method add(E item) of List_" + className);
+		
+		String[] newItems = {
+				"1","2","3","4","5","6","7","8","9","10",
+				"11","12","13","14","15","16","17","18","19","20",
+				"21","22","23","24","25","26","27","28","29","30",
+				"31","32","33","34","35","36","37","38","39","40",
+				"41","42","43","44","45","46","47","48","49","50",
+				"51","52","53","54","55","56","57","58","59","60",
+				"61","62","63","64","65","66","67","68","69","70",
+				"71","72","73","74","75","76","77","78","79","80",
+				"81","82","83","84","85","86","87","88","89","90",
+				"91","92","93","94","95","96","97","98","99","100"
+				};
+		
+		System.out.println("Add 100 items");
+		
+		for(String item: newItems) {
+			list.add(item);
+		}
+	}
 
 }
